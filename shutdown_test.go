1301aa4ac97e99fd82096acc6b112328f4c6159c
Yogesh <yogesh@lonkar.org>
2023-06-22 14:24:10 +0200

    Move args test to non windows GOOS

--- shutdown_test.go
+++ shutdown_test.go
@@ -3,10 +3,6 @@ package graceful
 import (
 	"context"
 	"errors"
-	"fmt"
-	"os"
-	"runtime"
-	"syscall"
 	"testing"
 	"time"
 
@@ -80,33 +76,4 @@ func TestShutdown_timeout(t *testing.T) {
 	if !errors.Is(err, observer.ErrTimeout) {
 		t.Errorf("expected '%v' to be in error tree, got '%v'", context.Canceled, err)
 	}
-}
-
-func TestShutdown_args(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		t.SkipNow()
-	}
-	go func() {
-		time.Sleep(delay)
-		NewObserver()
-		if err := trigger(syscall.SIGABRT); err != nil {
-			t.Errorf("expected nil, got %v", err)
-		}
-	}()
-	err := Shutdown(50*time.Millisecond, syscall.SIGABRT, syscall.SIGINT)
-	if err == nil {
-		t.Error("expected err, got nil")
-	}
-	if !errors.Is(err, observer.ErrTimeout) {
-		t.Errorf("expected '%v' to be in error tree, got '%v'", context.Canceled, err)
-	}
-}
-
-// trigger will send a syscall.SIGTERM signal to given process id.
-func trigger(sig syscall.Signal) error {
-	pid := os.Getpid()
-	if err := syscall.Kill(pid, sig); err != nil {
-		return fmt.Errorf("failed to send signal %s(%#v) to current process %d: %w", syscall.SIGTERM.String(), syscall.SIGTERM, pid, err)
-	}
-	return nil
 }
